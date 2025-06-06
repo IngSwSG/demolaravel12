cbd1a344304b55925bfa1f5cafc5dbbae01f8fad
GeraldFPA <GERALD.PALADINO@ucr.ac.cr>
2025-06-06 15:16:17 -0600

    se agrega test destroy

--- tests/Feature/DestroyTest.php
+++ tests/Feature/DestroyTest.php
@@ -1,30 +1,24 @@
 <?php
 
-namespace Tests\Feature;
-
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use App\Models\Task;
 use App\Models\User;
+use App\Models\Task;
+use function Pest\Laravel\delete;
+use function Pest\Laravel\route;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class DestroyTest extends TestCase
-{
-    use RefreshDatabase;
+uses(Tests\TestCase::class, RefreshDatabase::class);
 
-    /** @test */
-    public function destroy_deletes_task_and_redirects()
-    {
-        $user = User::factory()->create();
-        $task = Task::factory()->create([
-            'user_id' => $user->id,
-        ]);
+test('destroy deletes task and redirects', function () {
+    $user = User::factory()->create();
+    $task = Task::factory()->create([
+        'user_id' => $user->id,
+    ]);
 
-        $response = $this->delete(route('tasks.destroy', $task));
+    $response = delete(route('tasks.destroy', $task));
 
-        $response->assertRedirect(route('tasks.index'));
+    $response->assertRedirect(route('tasks.index'));
 
-        $this->assertDatabaseMissing('tasks', [
-            'id' => $task->id,
-        ]);
-    }
-}
+    $this->assertDatabaseMissing('tasks', [
+        'id' => $task->id,
+    ]);
+});

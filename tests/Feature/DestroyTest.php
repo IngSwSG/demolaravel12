5d9bc15515ff727a25905f6bf717ccfa090f55f4
GeraldFPA <GERALD.PALADINO@ucr.ac.cr>
2025-06-11 14:20:27 -0600

    se solucinan errores y se agrega campo para completar tasks

--- tests/Feature/DestroyTest.php
+++ tests/Feature/DestroyTest.php
@@ -2,23 +2,26 @@
 
 use App\Models\User;
 use App\Models\Task;
+use function Pest\Laravel\actingAs;
 use function Pest\Laravel\delete;
-use function Pest\Laravel\route;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-uses(Tests\TestCase::class, RefreshDatabase::class);
+uses(RefreshDatabase::class);
 
-test('destroy deletes task and redirects', function () {
+test('a user can delete their task', function () {
     $user = User::factory()->create();
     $task = Task::factory()->create([
         'user_id' => $user->id,
     ]);
 
-    $response = delete(route('tasks.destroy', $task));
+    actingAs($user);
+
+    $response = delete(route('tasks.destroy', $task->id));
 
     $response->assertRedirect(route('tasks.index'));
 
-    $this->assertDatabaseMissing('tasks', [
-        'id' => $task->id,
-    ]);
+    expect(Task::find($task->id))->toBeNull();
 });
+
+
+

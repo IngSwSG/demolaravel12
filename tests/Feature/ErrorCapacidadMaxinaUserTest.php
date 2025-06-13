41cb7f3cae393fa12a2d44a6827ddedd9596370e
VictorRodz <Victor.rodriguezarce@ucr.ac.cr>
2025-06-13 15:09:11 -0600

    Ya la prueba pasa, porque se arregla el error falso

---
+++ tests/Feature/ErrorCapacidadMaxinaUserTest.php
@@ -0,0 +1,12 @@
+<?php
+use App\Models\Team;
+use App\Models\User;
+
+
+it('no excede limite', function () {
+    $team = Team::factory()->create(['size' => 2]);
+    $users = User::factory(3)->create();
+
+    expect(fn() => $team->add($users))
+        ->toThrow(Exception::class, 'No puedes agregar más usuarios de los permitidos');
+});

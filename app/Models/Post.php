944a08145ec2eef160379d8451696c3039ad5007
kristel.cordoba <kristel.cordoba@ucr.ac.cr>
2025-06-18 14:35:23 -0600

    Implement Like functionality with models, migrations, and tests

---
+++ app/Models/Post.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
+
+class Post extends Model
+{
+    /** @use HasFactory<\Database\Factories\PostFactory> */
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function likes()
+    {
+        return $this->morphMany(Like::class, 'likeable');
+    }
+
+    public function like()
+    {
+        $this->likes()->create([
+            'user_id' => Auth::id()
+        ]);
+    }
+    
+}

61815d7b78544d7430c304059ef5bc46cc0a534c
JOSE ESTEBAN JARQUIN ESPINOZA <JOSE.JARQUINESPINOZA@ucr.ac.cr>
2025-06-18 14:12:06 -0600

    C23968

---
+++ app/Models/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Post extends Model
+{
+
+    use HasFactory;
+    
+    public function likes(): MorphMany
+    {
+        return $this->morphMany(Like::class, 'likeable');
+    }
+
+    public function like()
+    {
+        $this->likes()->create([
+            'user_id' => auth()->id(),
+        ]);
+    }
+}
